03fe6d17af91e945942100340dca006b8e459640
Arthur Hiew <ahfx8518@hotmail.com>
2021-03-12 16:12:04 -0800

    detail activity impl

--- app/src/main/java/com/example/stonks/ui/DetailActivity/StockDetailActivity.kt
+++ app/src/main/java/com/example/stonks/ui/DetailActivity/StockDetailActivity.kt
@@ -1,7 +1,7 @@
 package com.example.stonks.ui.DetailActivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.stonks.databinding.ActivityStockDetailBinding
 
 class StockDetailActivity : AppCompatActivity() {
@@ -10,7 +10,17 @@ class StockDetailActivity : AppCompatActivity() {
         val binding = ActivityStockDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.valueOpCashflowTv.text = "$11,111 m"
+        binding.valueDebtTv.text = "$11,111 m"
+        binding.valueInvestmentTv.text = "$11,111 m"
+        binding.valueNumShareTv.text = "$11,111 m"
+        binding.valueGrowthRate15Tv.text = "$11,111 m"
+        binding.valueGrowthRate510Tv.text = "$11,111 m"
+        binding.valueGrowthRate1120Tv.text = "$11,111 m"
+        binding.valueDiscountRateTv.text = "$11,111 m"
+        binding.valueLastCloseTv.text = "$11,111 m"
+        binding.valueIntrinsicTv.text = "$11,111 m"
+        binding.valueVerdictTv.text = "$11,111 m"
 
     }
 }
